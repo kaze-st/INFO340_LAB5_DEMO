590b3d2d779a1a49f46376ac6539b21df687cef5
Hao Chen <haoc46@cs.washington.edu>
2018-10-30 02:48:37 -0700

    Hi

--- demo/app.spec.js
+++ demo/app.spec.js
@@ -1,8 +1,32 @@
 'use strict'
-import {addTwoValues} from './js/app'
+import { addTwoValues, first } from './js/app'
 // test or it
 
-it("should add two numbers", function(){
-    // Assertion 
-    expect(addTwoValues(4, 5)).toEqual(9);
+describe("This tests addTwoValues", () => {
+    it("should add two positive numbers", function () {
+        // Assertion 
+        expect(addTwoValues(4, 5)).toEqual(9);
+        expect(addTwoValues(9, 1)).toEqual(10);
+        expect(addTwoValues(1, 10)).toEqual(11);
+    })
+
+    it("should add two negative numbers", function () {
+        // Assertion 
+        expect(addTwoValues(-1, -2)).toEqual(-3);
+        expect(addTwoValues(-9, -4)).toEqual(-13);
+    })
+
 })
+
+describe("This tests first", () => {
+    it("should return the first value", function () {
+        // Assertion 
+        expect(first([7, 4, 5, 3])).toEqual(7);
+        expect(first([4, 3, 2, 1], 3)).toContain(4);
+        expect(first([4, 3, 2, 1], 3)).toEqual([4, 3, 2]);
+        expect(first([4, 3, 2, 1], -3)).toEqual([]);
+    })
+
+
+})
+
--- demo/js/app.js
+++ demo/js/app.js
@@ -1,8 +1,17 @@
 'use strict'
 
 function addTwoValues(num1, num2){
-    return num1 + num2 - 60;
+    return num1 + num2;
 }
 
+function first(array, n) {
+    if (array == null) 
+    return void 0;
+  if (n == null) 
+    return array[0];
+  if (n < 0)
+    return [];
+  return array.slice(0, n);
+};
 
-export {addTwoValues};
+export {addTwoValues, first};
