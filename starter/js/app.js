d1ecb62c6792b7e2bc0edde0bef0849252ab8858
Hao Chen <haoc46@cs.washington.edu>
2018-10-30 08:40:31 -0700

    New

--- starter/js/app.js
+++ starter/js/app.js
@@ -4,5 +4,16 @@ function addTwoValues(num1, num2){
     return num1 + num2;
 }
 
+function first(array, n) {
+    if (array == null) 
+    return void 0;
+  if (n == null) 
+    return array[0];
+  if (n < 0)
+    return [];
+  return array.slice(0, n);
+};
+
+export {addTwoValues, first};
 
 export {addTwoValues};
